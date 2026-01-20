4e372d87876a948b7fb9c3f3c667dc1784c29ab0
Prashant <prashantkumar2531@gmail.com>
2026-01-20 20:14:11 +0530

    Add Spring Boot backend module and package structure

---
+++ Olx-Backend/src/main/java/com/olx/backend/OlxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.olx.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxBackendApplication.class, args);
+	}
+
+}
