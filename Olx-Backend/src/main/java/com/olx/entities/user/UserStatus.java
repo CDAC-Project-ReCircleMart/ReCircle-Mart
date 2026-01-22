8d542e753f1384c2e063e394e477207ff8ec00f9
harsh-kumar1 <harshkumar.mymail@gmail.com>
2026-01-22 13:44:07 +0530

    Login and register funcitonalities added to Prashant's spring project

---
+++ Olx-Backend/src/main/java/com/olx/entities/user/UserStatus.java
@@ -0,0 +1,24 @@
+package com.olx.entities.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Table(name = "user_status")
+@Getter
+public class UserStatus {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "status_id")
+	private Long statusId;
+
+	@Column(name = "status_name")
+	private String statusName;
+
+}
---
+++ Olx-Backend/src/main/java/com/olx/repository/user/UsersRepository.java
@@ -0,0 +1,14 @@
+package com.olx.repository.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.olx.entities.user.Users;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+
+    Optional<Users> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
