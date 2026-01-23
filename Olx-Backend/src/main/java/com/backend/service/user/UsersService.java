5a8447d6cee74e17b1168bc4c5aa9bbcf5322a04
harsh-kumar1 <harshkumar.mymail@gmail.com>
2026-01-23 19:20:28 +0530

    a

--- Olx-Backend/src/main/java/com/backend/service/user/UsersService.java
+++
@@ -1,9 +0,0 @@
-package com.olx.service.user;
-
-import com.olx.dtos.UserRegisterRequestDTO;
-import com.olx.entities.user.Users;
-
-public interface UsersService {
-
-    Users registerUser(UserRegisterRequestDTO request);
-}
